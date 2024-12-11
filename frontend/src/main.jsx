ee4397ea3131d9fe108368b7e8ec031995bba74e
Romina-Castillo <rominacastillo054@gmail.com>
2024-12-11 12:33:23 -0300

    instalo dependencias necesarias

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from "react-dom/client"
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
   </StrictMode>,
-)
+);
